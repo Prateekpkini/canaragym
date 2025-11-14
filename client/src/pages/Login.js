2cd5fdf6e2e51dbe63be211143e0f15abe23b55f
Prateekpkini <Prateekpkini2005@gmail.com>
2025-11-14 16:58:14 +0530

    Fixed with the Authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -19,7 +19,8 @@ function Login() {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+      const res = await axios.post(`${apiUrl}/api/auth/login`, {
         email,
         password,
       });
